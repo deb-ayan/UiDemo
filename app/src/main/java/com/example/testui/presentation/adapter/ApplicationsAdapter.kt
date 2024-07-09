b11c52f51b00e574e45556cf4a06ccc8dea80fb8
ayandebnath <rintu98deb@gmail.com>
2024-07-09 12:40:47 +0530

    check button logic

--- app/src/main/java/com/example/testui/presentation/adapter/ApplicationsAdapter.kt
+++ app/src/main/java/com/example/testui/presentation/adapter/ApplicationsAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.testui.presentation.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
